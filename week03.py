e7a22958e5ee1db0bfce7d87ff41a78a86989c15
leeminje <gtyfhvb58462@gmail.com>
2025-03-18 19:15:51 +0900

    week03 v1.2

--- week03.py
+++ week03.py
@@ -2,7 +2,8 @@
 drinks=["ice americano","cafe latte","Watermelon juice"]
 prices=[2000,3000,4900]
 total_price=0
-amounts=[0,0,0]
+amounts=[0 for _ in range(len(drinks))]
+
 
 def order_process(idx: int):
     global total_price
