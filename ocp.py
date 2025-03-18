4fc921a89337da60d9793de164fd8231b345f3fc
leeminje <gtyfhvb58462@gmail.com>
2025-03-18 20:22:37 +0900

    week03 ocp v0.2

--- ocp.py
+++ ocp.py
@@ -1,19 +1,24 @@
 import time
 
+def time_measure_decorate(f):
+    def wrapper(*args):
+        s=time.time()
+        r=f(*args)
+        e=time.time()
+        print(e-s)
+        return r
+    return wrapper
+
+@time_measure_decorate
 def one_to_n_loop(n):
-    s=time.time()
     result=0
     for i in range(1,n+1):
         result=result+i
-    e=time.time()
-    print(e-s)
     return result
 
+@time_measure_decorate
 def one_to_n_math(n):
-    s=time.time()
     r=n*(n+1)//2
-    e = time.time()
-    print(e - s)
     return r
 
 number=int(input("input number: "))
