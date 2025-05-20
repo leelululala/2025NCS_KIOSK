abf06ceab8f427c8be4d018b95c8a2c8187ddd75
leeminje <gtyfhvb58462@gmail.com>
2025-05-20 19:30:36 +0900

    week12 kiosk v4.0

--- week12.py
+++ week12.py
@@ -3,12 +3,6 @@ from kiosk import KioskGUI
 
 
 if __name__ == "__main__":
-    # url = f"https://wttr.in/incheon?&0&Q"
-    # response = requests.get(url)
-    # if response.status_code == 200:
-    #     print(response.text.strip())
-    # else:
-    #     print(f"상태 코드: {response.status_code}")
 
     menu_drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice", "Ice tea"]
     menu_prices = [2000, 3000, 4900, 3500]
